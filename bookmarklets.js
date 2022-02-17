e3849f5f1b8b3b528ca1b1f1d2c3b07542c7cbe9
Alban-Peyrat <77591243+Alban-Peyrat@users.noreply.github.com>
2022-02-17 16:08:05 +0100

    Validation automatique

--- bookmarklets.js
+++ bookmarklets.js
@@ -2,10 +2,12 @@
 javascript: (() => {
   document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_1")[0].value = "U00";
   document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_2")[0].value = "FAB";
+  document.getElementById("PAGE_BUTTONS_cbuttonsave").click()
 })();
 
 //Code stat odonto
 javascript: (() => {
   document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_1")[0].value = "U00";
   document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_2")[0].value = "FC0";
+  document.getElementById("PAGE_BUTTONS_cbuttonsave").click()
 })();
