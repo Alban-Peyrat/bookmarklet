ff00fcf32da184d8de3f6a071beeab0abbf499b0
Alban-Peyrat <77591243+Alban-Peyrat@users.noreply.github.com>
2022-02-17 16:02:05 +0100

    code stats

---
+++ bookmarklets.js
@@ -0,0 +1,11 @@
+//Code stat médecine
+javascript: (() => {
+  document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_1")[0].value = "U00";
+  document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_2")[0].value = "FAB";
+})();
+
+//Code stat odonto
+javascript: (() => {
+  document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_1")[0].value = "U00";
+  document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_2")[0].value = "FC0";
+})();
