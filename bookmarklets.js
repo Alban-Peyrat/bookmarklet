e1f479df42b31f94bb440e978fc41d56bce0e3de
Alban-Peyrat <77591243+Alban-Peyrat@users.noreply.github.com>
2022-03-16 15:51:19 +0100

    add IdRef

--- bookmarklets.js
+++ bookmarklets.js
@@ -11,3 +11,41 @@ javascript: (() => {
   document.getElementsByName("pageBean.itemMd.dnx.physicalItemTable.statisticsNote_2")[0].value = "FC0";
   document.getElementById("PAGE_BUTTONS_cbuttonsave").click()
 })();
+
+//IdRef recherche personne
+javascript: (() => {
+  var list = document.getElementsByName("ComboIndex");
+  
+  for(var ii=0; ii<list.length; ii++) {
+    if(list[ii].value == "persname_t:#val#"){
+      list[ii].checked = true;
+    }
+  }
+  document.getElementById("Text1").focus();
+})();
+
+//IdRef recherche experte nom de famille
+javascript: (() => {
+  var list = document.getElementsByName("ComboIndex");
+  document.getElementById("Text1").value = "nom_t:"
+  
+  for(var ii=0; ii<list.length; ii++) {
+    if(list[ii].value == "#val#"){
+      list[ii].checked = true;
+    }
+  }
+  document.getElementById("Text1").focus();
+})();
+
+//IdRef recherche experte nom de famille et prénom
+javascript: (() => {
+  var list = document.getElementsByName("ComboIndex");
+  document.getElementById("Text1").value = "nom_t: AND prenom_t:"
+  
+  for(var ii=0; ii<list.length; ii++) {
+    if(list[ii].value == "#val#"){
+      list[ii].checked = true;
+    }
+  }
+  document.getElementById("Text1").focus();
+})();
